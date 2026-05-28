7f7b6118d1b2b0d62a0b3d3a0e6bf120f5e58084
Debbido27 <davidmedina272004@gmail.com>
2026-05-27 20:05:15 -0600

    Creacion de atributos para jugador

--- src/LOGIC/USER.java
+++ src/LOGIC/USER.java
@@ -1,6 +1,15 @@
 
 package LOGIC;
 
+import java.time.LocalDate;
+
 public class USER {
-   
+    
+    //ATRIBUTOS JUGADOR
+   private String username;
+   private String password;
+   private String fullname;
+   private LocalDate fechaRegistro;
+   private long ultimaSesion;
+   private String avatarPath;
 }
