700e6256a57d395aeb058613d3a3ce620ef34843
Debbido27 <davidmedina272004@gmail.com>
2026-05-27 20:27:37 -0600

    Clase creada login manager, y atributos

---
+++ src/LOGIC/LoginManager.java
@@ -0,0 +1,12 @@
+
+package LOGIC;
+
+
+public class LoginManager {
+    private static final String BASE_FOLDER = "CTR_RAIZ";
+    private static final String USERS_FILE  = BASE_FOLDER + "/users.ctr";
+    private static final String USERS_TEMP  = BASE_FOLDER + "/users_temp.ctr";
+
+    private USER currentUser;
+    
+}
