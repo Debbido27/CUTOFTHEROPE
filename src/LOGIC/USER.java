0e44b9e0d93dd9d03dcd78a3f6ea02ab0ea9b4bf
Debbido27 <davidmedina272004@gmail.com>
2026-05-27 20:21:05 -0600

    Constructor

--- src/LOGIC/USER.java
+++ src/LOGIC/USER.java
@@ -20,9 +20,9 @@ public class USER {
    private int[] puntajesPorNivel;
    private long tiempoTotalJugado;
    private int partidasJugadas;
-   private int fallosTotal;
+   private int fallosTotales;
    private int estrellasTotal;
-   private int puntuacionGenera;
+   private int puntuacionGeneral;
    
    //PREFERENCIAS
    private float volumen;
@@ -30,7 +30,23 @@ public class USER {
    //Social 
    private String[] amigos;
    
-   
-   
-   
+    public USER(String username, String password, String fullname) {
+          this.username = username;
+          this.password = password;
+          this.fullname = fullname;
+          this.fechaRegistro = LocalDate.now();
+          this.ultimaSesion = System.currentTimeMillis();
+          this.avatarPath = "";
+          this.nivelActual = 1;
+          this.nivelesDesbloqueados = new boolean[]{true, false, false, false, false};
+          this.puntajesPorNivel = new int[5];
+          this.tiempoTotalJugado = 0;
+          this.partidasJugadas = 0;
+          this.fallosTotales = 0;
+          this.estrellasTotal = 0;
+          this.puntuacionGeneral = 0;
+          this.volumen = 1.0f;
+          this.amigos = new String[0];
+      }
+
 }
