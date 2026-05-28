dc20a8b85f8f8a3e59d6a91b998df005af87faac
Debbido27 <davidmedina272004@gmail.com>
2026-05-27 20:41:39 -0600

    Metodo para oder leer registro

--- src/LOGIC/LoginManager.java
+++ src/LOGIC/LoginManager.java
@@ -3,6 +3,8 @@ package LOGIC;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.time.LocalDate;
 
 
 public class LoginManager {
@@ -19,4 +21,51 @@ public class LoginManager {
         limpiarUsuariosHuerfanos();
         currentUser = cargarUltimoUsuarioActivo();
     }
+    
+    
+            private USER leerRegistro(RandomAccessFile f) throws IOException {
+            String username   = f.readUTF();
+            String password   = f.readUTF();
+            String fullname   = f.readUTF();
+            long fechaReg     = f.readLong();
+            long ultimaSesion = f.readLong();
+            String avatarPath = f.readUTF();
+            int nivelActual   = f.readInt();
+
+            boolean[] niveles = new boolean[5];
+            for (int i = 0; i < 5; i++) niveles[i] = f.readBoolean();
+
+            int[] puntajes = new int[5];
+            for (int i = 0; i < 5; i++) puntajes[i] = f.readInt();
+
+            long tiempoTotal = f.readLong();
+            int partidas     = f.readInt();
+            int fallos       = f.readInt();
+            int estrellas    = f.readInt();
+            int puntuacion   = f.readInt();
+            float volumen    = f.readFloat();
+            String idioma    = f.readUTF();
+
+            int totalAmigos  = f.readInt();
+            String[] amigos  = new String[totalAmigos];
+            for (int i = 0; i < totalAmigos; i++) amigos[i] = f.readUTF();
+
+            USER  u = new USER(username, password, fullname);
+            u.setFechaRegistro(LocalDate.ofEpochDay(fechaReg));
+            u.setUltimaSesion(ultimaSesion);
+            u.setAvatarPath(avatarPath);
+            u.setNivelActual(nivelActual);
+            for (int i = 0; i < 5; i++) if (niveles[i]) u.desbloquearNivel(i);
+            for (int i = 0; i < 5; i++) u.setPuntajeNivel(i, puntajes[i]);
+            u.setTiempoTotalJugado(tiempoTotal);
+            u.setPartidasJugadas(partidas);
+            u.setFallosTotales(fallos);
+            u.setEstrellasTotal(estrellas);
+            u.setPuntuacionGeneral(puntuacion);
+            u.setVolumen(volumen);
+            u.setAmigos(amigos);
+            return u;
+        }
+
+    
 }
