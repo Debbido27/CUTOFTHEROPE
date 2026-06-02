5c53f432e37fc7855bcdf631e8b8fd9e65332127
Debbido27 <davidmedina272004@gmail.com>
2026-06-01 18:11:46 -0600

    atrbiutos y colores

--- core/src/main/java/com/cutherope/PerfilScreen.java
+++ core/src/main/java/com/cutherope/PerfilScreen.java
@@ -4,7 +4,25 @@
  */
 package com.cutherope;
 
+import LOGIC.LoginManager;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
-public class PerfilScreen {
+
+public class PerfilScreen implements Screen{
     
+    private CutTheRope   juego;
+    private String       usuario;
+    private LoginManager gestor;
+    private Stage        escenario;
+    private Skin         piel;
+
+    private final Color FONDO   = new Color(0.96f, 0.92f, 0.82f, 1f);
+    private final Color VERDE   = new Color(0.33f, 0.59f, 0.31f, 1f);
+    private final Color CAFE    = new Color(0.23f, 0.16f, 0.08f, 1f);
+    private final Color NARANJA = new Color(0.78f, 0.63f, 0.31f, 1f);
+    private final Color ROJO    = new Color(0.70f, 0.27f, 0.20f, 1f);
+    private final Color GRIS    = new Color(0.60f, 0.60f, 0.60f, 1f);
 }
