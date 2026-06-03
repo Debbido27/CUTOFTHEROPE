53466e981e356761ff8532b38da8f35a9349f6af
Debbido27 <davidmedina272004@gmail.com>
2026-06-03 14:04:21 -0600

    Metodos para poder obtener el path de los amigos, y el get para solicitar el path

--- core/src/main/java/LOGIC/FriendsManager.java
+++ core/src/main/java/LOGIC/FriendsManager.java
@@ -1,7 +1,23 @@
 
 package LOGIC;
 
+import java.io.File;
+import java.io.IOException;
+
 
 public class FriendsManager {
     
+    private static final String BASE_FOLDER ="../assets/CTR._RAIZ";
+    
+    private String getAmigosPath(String username){
+        return BASE_FOLDER+"/"+username+"/amigos.ctr";
+    }
+    
+    private String getSolicitudesPath(String username) {
+        File f = new File(BASE_FOLDER + "/" + username + "/solicitudes.ctr");
+        if (!f.exists()) {
+            try { f.createNewFile(); } catch (IOException e) {}
+        }
+        return f.getPath();
+    }
 }
