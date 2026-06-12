fbd22378e2888e4222ff48b4d8add39a21b2ced9
Debbido27 <davidmedina272004@gmail.com>
2026-06-12 14:56:18 -0600

    Constructor y clase estrella creada

--- core/src/main/java/Game/Estrella.java
+++ core/src/main/java/Game/Estrella.java
@@ -4,10 +4,18 @@
  */
 package Game;
 
+import com.badlogic.gdx.graphics.Texture;
+
 /**
  *
  * @author Dell
  */
-public class Estrella {
-    
+public class Estrella extends ElementoNivel implements Interactuable {
+ private boolean activa;
+ 
+ public Estrella(float x, float y, float ancho, float alto, Texture textura) {
+        super(x, y, ancho, alto, TipoElemento.ESTRELLA, textura);
+        this.activa = true;
+    }
+ 
 }
