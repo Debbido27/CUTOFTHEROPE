46e5d67d8d81f8db9b448d8481c7cecef80377ab
Debbido27 <davidmedina272004@gmail.com>
2026-06-15 20:30:03 -0600

    Clase musica creada donde se pondra la musica en loop durante el juego

---
+++ core/src/main/java/LOGIC/MusicManager.java
@@ -0,0 +1,52 @@
+package com.cutherope;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+
+public class MusicaManager {
+
+    private static MusicaManager instancia;
+    private Music musica;
+
+    private MusicaManager() {
+        musica = Gdx.audio.newMusic(Gdx.files.internal("audio/Cut the Rope Theme.mp3"));
+        musica.setLooping(true);
+        musica.setVolume(1.0f);
+    }
+
+    public static MusicaManager getInstance() {
+        if (instancia == null) instancia = new MusicaManager();
+        return instancia;
+    }
+
+    public void reproducir() {
+        if (!musica.isPlaying()) musica.play();
+    }
+
+    public void pausar() {
+        if (musica.isPlaying()) musica.pause();
+    }
+
+    public void setVolumen(float vol) {
+        musica.setVolume(vol);
+    }
+
+    public float getVolumen() {
+        return musica.getVolume();
+    }
+
+    public boolean isReproduciendo() {
+        return musica.isPlaying();
+    }
+
+    public void aplicarPreferencias(boolean activa, float volumen) {
+        setVolumen(volumen);
+        if (activa) reproducir();
+        else        pausar();
+    }
+
+    public void dispose() {
+        if (musica != null) musica.dispose();
+        instancia = null;
+    }
+}
